42de47350ef1a0dcaa7345a75df2a5a94f59e21a
dhruv2472 <d.r.panchal029@gmail.com>
2023-10-30 20:31:05 +0530

    watch notification code

---
+++ app/src/main/java/com/example/watchnotification/presentation/fcm/FirebaseNotification.kt
@@ -0,0 +1,61 @@
+package com.example.watchnotification.presentation.fcm
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_IMMUTABLE
+import android.content.Intent
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.example.watchnotification.R
+import com.example.watchnotification.presentation.MainActivity
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import kotlin.random.Random
+
+
+class FirebaseNotification : FirebaseMessagingService(){
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+        Log.d("Token", token)
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        message.notification?.let {
+            sendNotification(it)
+        }
+
+    }
+
+    private fun sendNotification(remoteMessage: RemoteMessage.Notification) {
+        val title: String = remoteMessage.title.toString()
+        val text: String = remoteMessage.body.toString()
+
+        val intent = Intent(this, MainActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP).putExtra("message", text)
+
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, FLAG_IMMUTABLE)
+
+        val channelId = this.resources.getString(R.string.default_notification_channel_id)
+        val channelName = this.resources.getString(R.string.default_notification_channel_name)
+
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setContentTitle(title)
+            .setContentText(text)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+
+        val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val channel = NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            manager.createNotificationChannel(channel)
+        }
+        manager.notify(Random.nextInt(), notification.build())
+    }
+}
